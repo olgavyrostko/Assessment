ff3852251a3d64a9db940b4c386a23c024d3113b
Kirill Chuyanov <kirill.chuyanov@bolt.eu>
2021-10-18 18:56:17 +0300

    Initial Commit

---
+++ Assesment/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Assesment
+//
+//  Created by Kirill Chuyanov on 18.10.2021.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tartu = CLLocationCoordinate2D(latitude: 58.370850, longitude: 26.714720)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = tartu
+        map.addAnnotation(annotation)
+        map.setRegion(MKCoordinateRegion(center: tartu, latitudinalMeters: 8_000, longitudinalMeters: 8_000), animated: true)
+    }
+}
+
